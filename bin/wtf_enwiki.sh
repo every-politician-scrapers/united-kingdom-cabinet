de65dabf7bfb420f531656de94a3787dc5f25755
Tony Bowden <tony@tmtm.com>
2021-11-30 09:03:23 +0200

    update wtf_wikipedia action

--- bin/wtf_enwiki.sh
+++ bin/wtf_enwiki.sh
@@ -4,9 +4,9 @@ IFS=$'\n'
 
 mkdir -p enwiki
 
-for page in $(qsv select enwiki html/current.csv | qsv search . | qsv behead); do
+for page in $(qsv select enwiki html/current.csv | qsv search . | qsv dedup | qsv sort | qsv behead); do
   echo $page
-  json=$(wtf_wikipedia $page)
+  json=$(printf '"%s"' "$page" | xargs wtf_wikipedia)
   pageid=$(printf '%s' "$json" | jq -r .pageID)
-  printf '%s' "$json" | jq -r . > enwiki/$pageid
+  printf '%s' "$json" | jq -r '.sections |= [.[0]]' > enwiki/$pageid
 done
