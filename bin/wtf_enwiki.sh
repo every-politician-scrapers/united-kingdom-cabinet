1fe35de4c3d90c28c479424e81ac0e3b3e2097be
Tony Bowden <tony@tmtm.com>
2021-12-08 07:05:22 +0200

    skip QEII in wtf_wikipedia runs

--- bin/wtf_enwiki.sh
+++ bin/wtf_enwiki.sh
@@ -4,7 +4,7 @@ IFS=$'\n'
 
 mkdir -p enwiki
 
-for page in $(qsv select enwiki html/current.csv | qsv search . | qsv dedup | qsv sort | qsv behead); do
+for page in $(fgrep -v Q9682 html/current.csv | qsv select enwiki | qsv search . | qsv dedup | qsv sort | qsv behead); do
   echo $page
   json=$(printf '"%s"' "$page" | xargs wtf_wikipedia)
   pageid=$(printf '%s' "$json" | jq -r .pageID)
