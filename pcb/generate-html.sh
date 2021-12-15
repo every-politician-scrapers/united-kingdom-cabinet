6d88caacc40b891b4fd0d8676559b75c79acad47
Tony Bowden <tony@tmtm.com>
2021-12-15 10:37:48 +0200

    Update to latest PCB

--- pcb/generate-html.sh
+++ pcb/generate-html.sh
@@ -35,7 +35,7 @@ jq -r 'def highest(array): (array | sort_by(.rank) | reverse | first.value);
     highest(.claims.P18),
     (try (.sitelinks.enwiki) catch null)
   ] | @csv' $RAWBIOS |
-  sed -e 's/Q6581097/male/' -e 's/Q6581072/female/' >> $BIO_CSV
+  sed -e 's/Q6581097/male/' -e 's/Q6581072/female/' -e 's/Q1052281/female/' >> $BIO_CSV
 
 # TODO: other positions
 # TODO: relations
@@ -104,13 +104,13 @@ if [ ${#warnings[@]} -gt 0 ]; then
   printf '* %s\n' "${warnings[@]}"
 fi
 
-warnings=($(qsv join --left-anti prev $EXTD_21 personID html/holders21.csv | qsv search -s start "^2" | qsv search -s prev . | qsv select prev,position,start | qsv sort -s start -R | qsv behead | qsv table))
+warnings=($(qsv join --left-anti prev $EXTD_21 personID html/holders21.csv | qsv search -s start "^2" | qsv search -s prev . | qsv select prev,position,start,personID | qsv sort -s start -R | qsv behead | qsv table))
 if [ ${#warnings[@]} -gt 0 ]; then
   echo "## Missing predecessors:"
   printf '* %s\n' "${warnings[@]}"
 fi
 
-warnings=($(qsv join --left-anti next $EXTD_21 personID html/holders21.csv | qsv search -s next . | qsv select next,position,end | qsv sort -s end -R | qsv behead | qsv table))
+warnings=($(qsv join --left-anti next $EXTD_21 personID html/holders21.csv | qsv search -s next . | qsv select next,position,end,personID | qsv sort -s end -R | qsv behead | qsv table))
 if [ ${#warnings[@]} -gt 0 ]; then
   echo "## Missing successors:"
   printf '* %s\n' "${warnings[@]}"
