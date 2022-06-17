c6226a620296a8c669558de8618627006c9d56ec
Tony Bowden <tony@tmtm.com>
2022-06-17 12:15:46 +0300

    update to latest wpoffice

--- sources/ambassador-uae/scraper.rb
+++ sources/ambassador-uae/scraper.rb
@@ -4,50 +4,21 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
-  def empty?
-    too_early?
-  end
-
-  def combo_date?
-    true
-  end
-
-  def raw_combo_date
-    raise 'need to define a raw_combo_date'
-  end
-
-  def name_node
-    raise 'need to define a name_node'
-  end
-
-  def too_early?
-    false
-  end
-end
-
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
-  # decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  # TODO: make this easier to override
   def holder_entries
     noko.xpath("//h3[.//span[contains(.,'Ambassadors')]]//following-sibling::ul[1]//li")
   end
 
-  class Officeholder < OfficeholderNonTable
+  class Officeholder < OfficeholderNonTableBase
+    def name_node
+      noko.css('a').last
+    end
+
     def raw_combo_date
       noko.text.split(':').first
-    end
-
-    def itemLabel
-      noko.text.split(':').last.tidy
-    end
-
-    def item
-      noko.css('a/@wikidata').map(&:text).last
     end
   end
 end
