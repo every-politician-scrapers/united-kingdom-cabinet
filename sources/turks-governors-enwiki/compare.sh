bc6ed74f4adfb47c980137f1ec9ae532577979dd
Tony Bowden <tony@tmtm.com>
2022-06-17 13:21:16 +0300

    update to latest wpoffice

--- sources/turks-governors-enwiki/compare.sh
+++ sources/turks-governors-enwiki/compare.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-bundle exec ruby scraper.rb $(jq -r .source meta.json) > scraped.csv
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s startDate > wikidata.csv
-bundle exec ruby diff.rb | qsv sort -s startdate | tee diff.csv
+curl -LsS https://github.com/every-politician-scrapers/shared/raw/main/wpoffice.tgz | tar xfz - --strip-components=1
+bash run-comparison.sh
+git clean -qfX
 
 cd ~-
