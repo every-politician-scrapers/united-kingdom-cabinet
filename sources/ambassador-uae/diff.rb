486c23c0864b8355f1283549fd99c376ed14e940
Tony Bowden <tony@tmtm.com>
2022-06-17 12:15:33 +0300

    update to latest wpoffice

--- sources/ambassador-uae/diff.rb
+++ sources/ambassador-uae/diff.rb
@@ -3,5 +3,8 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
+diff = EveryPoliticianScraper::Comparison.new('wikidata.csv', 'scraped.csv').diff
+diff = DaffDiff::Decorator.new(data: diff, cell_class: DaffDiff::Decorator::DatePrecision).decorated
+diff = DaffDiff::Decorator.new(data: diff, cell_class: DaffDiff::Decorator::Nullless).decorated
+
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
