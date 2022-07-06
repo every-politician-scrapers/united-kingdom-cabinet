2a42d1742a9c6e2a7e746f940e03605feadd2534
Tony Bowden <tony@tmtm.com>
2022-07-06 06:42:07 +0300

    compare enwiki Solicitors General

---
+++ sources/wpoffice/solicitor-general/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  def table_number
+    'position()>=0'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ name img start end].freeze
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
