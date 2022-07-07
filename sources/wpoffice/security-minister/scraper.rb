59731226747f94694fd713fd78289b8090d34dfd
Tony Bowden <tony@tmtm.com>
2022-07-07 10:12:29 +0300

    compare enwiki Security ministers

---
+++ sources/wpoffice/security-minister/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[_ name img start end].freeze
+    end
+
+    field :title do
+      tds[1].text[/\((.*?)\)/, 1].tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
