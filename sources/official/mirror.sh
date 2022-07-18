a205c0f90c32f59ea8d7d7d4dbce727ba1ebddb9
Tony Bowden <tony@tmtm.com>
2022-07-18 11:15:23 +0300

    update mirror.sh for new meta.json

--- sources/official/mirror.sh
+++ sources/official/mirror.sh
@@ -2,10 +2,10 @@
 
 cd $(dirname $0)
 
-if [[ $(jq -r .source.url meta.json) == http* ]]
+if [[ $(jq -r .reference.P854 meta.json) == http* ]]
 then
   CURLOPTS='-L -c /tmp/cookies -A eps/1.2'
-  curl $CURLOPTS -o official.html $(jq -r .source.url meta.json)
+  curl $CURLOPTS -o official.html $(jq -r .reference.P854 meta.json)
 fi
 
 cd ~-
